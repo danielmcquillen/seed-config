60f98d94fc55a5ffd726cc8b96f4edc7ddc6d57f
danielmcquillen <daniel@mcquilleninteractive.com>
2016-03-02 10:22:45 +1100

    A variables of the start-seed.sh from the main project

---
+++ bin/start-seed.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# Start SEED in developer mode
+
+export DJANGO_SETTINGS_MODULE=config.settings.dev
+cd ~/seed
+
+# check if "$prog" is running
+running () {
+    prog="$1"
+    #printf "Looking for '$prog'\n"
+    n=$(/bin/ps auxw | grep -c "$prog")
+    if [ $n -lt 2 ]; then
+        ret=0
+    else
+        ret=1
+    fi
+    return $ret
+}
+
+# Redis
+running redis
+if [ $? -eq 0 ]; then
+    printf "Starting Redis server\n"
+    redis-server >/tmp/redis-server.log 2>&1 &
+else
+    printf "Redis is already running\n"
+fi
+
+# Celery
+running celery
+if [ $? -eq 0 ]; then
+    printf "Starting Celery\n"
+    celery -A seed worker -l info -c 4 --maxtasksperchild 1000 --events > /tmp/celeryd.log 2>&1 &
+else
+    printf "Celery is already running\n"
+fi
+
+# Django standalone server
+running "manage.py runserver"
+if [ $? -eq 0 ]; then
+    printf "Starting Django standalone server\n"
+    ./manage.py runserver 0.0.0.0:8000 --settings=config.settings.dev
+else
+    printf "Django server is already running\n"
+fi
